2531004688e54f4e1089c750a2472058e3ce9f14
kirtan kaushik <kirtankaushik54@gmail.com>
2023-11-27 18:52:35 +0530

    list

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hamBurger = document.querySelector('.hamburger');
 const nav = document.querySelector('.navBar');
-const navBar = document.querySelector('ul');
+const navBar = document.querySelector('.allScreen');
 const bar = document.querySelector('.bars');
 
 hamBurger.addEventListener('click', () => {
@@ -10,6 +10,19 @@ hamBurger.addEventListener('click', () => {
     }else{
         navBar.style.top = '-80px';
         bar.innerHTML = '&#9776;';
+    }
+});
+
+const mobilBar = document.querySelector('.mobBarList');
+const button = document.querySelector('.bar');
+
+button.addEventListener('click', () => {
+    if (mobilBar.style.top === '-250px' || mobilBar.style.top === '') {
+        mobilBar.style.top = '20px';
+        button.innerHTML = '&#10006;';
+    } else {
+        mobilBar.style.top = '-250px'; // Adjust this value based on your design
+        button.innerHTML = '&#9776;';
     }
 });
 
